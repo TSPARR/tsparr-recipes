26d1691f2db6accdcd1e2c25a902a3c853d6fe35
Tyler Sparr <tyler.sparr@Tyler-Sparr.local>
2016-12-30 12:14:18 -0500

    Added Microsoft Silverlight Recipes

---
+++ Microsoft Silverlight/Uninstall Microsoft Silverlight.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -rf "/Library/Internet Plug-Ins/Silverlight.plugin"
+rm -rf "/Library/Receipts/Silverlight.pkg"
+rm -rf "/Library/Receipts/Silverlight_W2_MIX.pkg"
+rm -rf "/Library/Internet Plug-Ins/WPFe.plugin"
+rm -rf "/Library/Receipts/WPFe.pkg"
