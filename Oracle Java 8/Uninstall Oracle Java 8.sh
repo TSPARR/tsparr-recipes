c91a1fda6b7bbf1e62bc89547805e694ec9b61fe
Tyler Sparr <tyler.sparr@Tyler-Sparr.local>
2017-01-05 16:41:24 -0500

    Lots of additions and changes

---
+++ Oracle Java 8/Uninstall Oracle Java 8.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin 
+rm -rf /Library/PreferencePanes/JavaControlPanel.prefPane
+rm -rf ~/Library/Application\ Support/Java
+
+exit 0
