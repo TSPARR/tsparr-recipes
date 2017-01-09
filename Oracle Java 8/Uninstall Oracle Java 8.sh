987432bd021189ad24dddd1aa332e9ef22e33166
Tyler Sparr <tyler.sparr@Tyler-Sparr.local>
2017-01-09 15:57:06 -0500

    Haven't committed in awhile

--- Oracle Java 8/Uninstall Oracle Java 8.sh
+++ Oracle Java 8/Uninstall Oracle Java 8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin 
+rm -rf /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin
 rm -rf /Library/PreferencePanes/JavaControlPanel.prefPane
-rm -rf ~/Library/Application\ Support/Java
+rm -rf /Users/$3/Library/Application\ Support/Java
 
 exit 0
